a79109eb2cda96920659b71a3fcd9d586589c6ae
Bearz-Bogdan <biarzbogdan@yahoo.com>
2024-09-04 17:01:29 +0300

    Delete src directory

--- src/constants.js
+++
@@ -1,7 +0,0 @@
-export const BASE_URL = '';
-export const PRODUCTS_URL = `/api/products`;
-export const ORDER_URL = `/api/orders`;
-export const USERS_URL = `/api/users`;
-export const PAYPAL_URL = `/api/config/paypal`;
-export const STRIPE_URL = `/api/config/stripe`;
-export const UPLOAD_URL = `/api/upload`;
